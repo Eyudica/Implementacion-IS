7b52afa64043f784ae50420dbe447bf30899b462
Eyudica <164026957+Eyudica@users.noreply.github.com>
2026-07-27 20:04:43 -0300

    Update AlreadyFriendsException.java

--- backend/src/main/java/com/umbook/exception/AlreadyFriendsException.java
+++ backend/src/main/java/com/umbook/exception/AlreadyFriendsException.java
@@ -2,7 +2,7 @@ package com.umbook.exception;
 
 public class AlreadyFriendsException extends RuntimeException {
     public AlreadyFriendsException() {
-        super("Los usuarios ya son amigos");
+        super("Ya son amigos.");
     }
     public AlreadyFriendsException(String message) {
         super(message);
